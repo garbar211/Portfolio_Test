af52bc724c263dc3a4ad679d7af733e91f2593f1
garbar 21 <91475415+garbar211@users.noreply.github.com>
2023-04-04 14:17:52 +0000

    Type Again

--- src/components/MyLife/TypeLife.js
+++ src/components/MyLife/TypeLife.js
@@ -7,12 +7,25 @@ function Type() {
       options={{
         strings: [
           "Wow! He is really cool! -<a href='https://www.google.com' style='color: blue; font-weight: bold;'> Mack</a>",
-          "Wow! He is really REALLY COOL! -<a href='https://www.google.com' style='color: blue; font-weight: bold;'> Bob</a>",
+          "Wow! He is really REALLY COOL! -<a href='https://www.google.com' style='color: blue; font-weight: bold;'> <a href='https://www.google.com' style='color: blue; font-weight: bold;'>Bob</a></a>",
         ],
         autoStart: true,
         loop: true,
         deleteSpeed: 50,
         escapeHtml: false, // set escapeHtml to true
+      }}
+      onInit={(typewriter) => {
+        typewriter
+          .pauseFor(2000)
+          .callFunction(() => {
+            const links = document.querySelectorAll("a");
+            links.forEach((link) => {
+              link.addEventListener("click", () => {
+                window.open(link.href, "_blank");
+              });
+            });
+          })
+          .start();
       }}
     />
   );
