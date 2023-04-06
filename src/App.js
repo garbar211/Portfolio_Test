69500f0db24673d85237a74e4373952236865d85
garbar 21 <91475415+garbar211@users.noreply.github.com>
2023-04-06 16:18:13 +0000

    Rollback

--- src/App.js
+++ src/App.js
@@ -17,7 +17,6 @@ import ScrollToTop from "./components/ScrollToTop";
 import "./style.css";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { Analytics } from '@vercel/analytics/react';
 
 function App() {
   const [load, upadateLoad] = useState(true);
@@ -34,19 +33,17 @@ function App() {
     <Router>
       <Preloader load={load} />
       <div className="App" id={load ? "no-scroll" : "scroll"}>
-        <Analytics id={process.env.VERCEL_ANALYTICS_ID}>
-          <Navbar />
-          <ScrollToTop />
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/project" element={<Projects />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/resume" element={<Resume />} />
-            <Route path="/mylife" element={<MYLife />} />
-            <Route path="*" element={<Navigate to="/"/>} />
-          </Routes>
-          <Footer />
-        </Analytics>
+        <Navbar />
+        <ScrollToTop />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/project" element={<Projects />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/resume" element={<Resume />} />
+          <Route path="/mylife" element={<MYLife />} />
+          <Route path="*" element={<Navigate to="/"/>} />
+        </Routes>
+        <Footer />
       </div>
     </Router>
   );
