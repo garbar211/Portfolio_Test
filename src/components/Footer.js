1278234f7093acda821da4c3ffc3351d4a1ac816
garbar 21 <91475415+garbar211@users.noreply.github.com>
2023-04-12 14:46:11 +0000

    Footer FIxed

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -14,7 +14,7 @@ function Footer() {
     <Container fluid className="footer">
       <Row>
         <Col md="4" className="footer-copywright">
-          <h3><a>https://github.com/garbar211/Portfolio_Test/graphs/commit-activity</a>Track commits</h3>
+          <h3>Not FInished </h3>
         </Col>
         <Col md="4" className="footer-copywright">
           <h3>Copyright © {year}</h3>
