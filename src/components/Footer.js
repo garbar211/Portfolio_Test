6da22d73e620304c137c9de0c30b82f1c1ba2f62
garbar 21 <91475415+garbar211@users.noreply.github.com>
2023-04-17 13:54:11 +0000

    Hopefully fixed index

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -14,7 +14,7 @@ function Footer() {
     <Container fluid className="footer">
       <Row>
         <Col md="4" className="footer-copywright">
-          <h3>Not FInished </h3>
+          <h3>Not Finshed </h3>
         </Col>
         <Col md="4" className="footer-copywright">
           <h3>Copyright © {year}</h3>
