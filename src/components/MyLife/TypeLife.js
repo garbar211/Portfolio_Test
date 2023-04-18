ce507aa1037eff781820f7f931c4029a6bd938d3
Garbar2121 <10470800-Garbar2121@users.noreply.replit.com>
2023-04-18 15:04:42 +0000

    Type AGAIN?

--- src/components/MyLife/TypeLife.js
+++ src/components/MyLife/TypeLife.js
@@ -9,7 +9,8 @@ function Type() {
         strings: [
           `<img src="${homeLogo}" alt="Test 1"/><br/><b style="text-align:center;">This dude is kinda cool I guess... -John Doe</b>`,
           `<img src="${homeLogo}" alt="Test 2"/><br/><b style="text-align:center;">He does cool stuff sometimes... - Jane Doe</b>`,
-          `<img src="${homeLogo}" alt="Test 3"/><br/><b style="text-align:center;">I mean.. he is ok... - Bob</b>`
+          `<img src="${homeLogo}" alt="Test 3"/><br/><b style="text-align:center;">I mean.. he is ok... - Bob</b>`,
+           `<img src="${homeLogo}" alt="Test 3"/><br/><b style="text-align:center;">Maybe I Was too hard on him.. - John doe (AGAIN)</b>`
         ],
         autoStart: true,
         loop: true,
