f73a3cd97745b0d46eec10aa0fb0706532bfe1bf
garbar211 <91475415+garbar211@users.noreply.github.com>
2023-04-25 15:08:31 +0000

    Type updated, removed image, I think it's too much..

--- src/components/MyLife/TypeLife.js
+++ src/components/MyLife/TypeLife.js
@@ -7,8 +7,8 @@ function Type() {
     <Typewriter
       options={{
         strings: [
-          `<img src="${homeLogo}" alt="Test 1"/><br/><b style="text-align:center;">Garret has demonstrated a strong work ethic, leadership skills, and a positive attitude. He has earned the respect and admiration of his peers and teachers alike.  - Sarah Schroeder </b>`,
-          `<img src="${homeLogo}" alt="Test 1"/><br/> <b style="text-align:center;">He is a trustworthy young man that takes pride in his work and means the best for all he encounters. I am confident that he will be an asset in his future workplace environment.  - Sarah Schroeder</b>`,
+          `<br/><b style="text-align:center;"> - Sarah Schroeder - Garret has demonstrated a strong work ethic, leadership skills, and a positive attitude. He has earned the respect and admiration of his peers and teachers alike. </b>`,
+          `9<br/> <b style="text-align:center;">Sarah Schroeder - He is a trustworthy young man that takes pride in his work and means the best for all he encounters. I am confident that he will be an asset in his future workplace environment. </b>`,
          
         ],
         autoStart: true,
