b2cb53db74ebfe300db4ecfb5e46179bde74ab7e
garbar211 <91475415+garbar211@users.noreply.github.com>
2023-04-25 14:27:57 +0000

    Type fix

--- src/components/MyLife/TypeLife.js
+++ src/components/MyLife/TypeLife.js
@@ -7,9 +7,9 @@ function Type() {
     <Typewriter
       options={{
         strings: [
-          `<img src="${homeLogo}" alt="PG Logo"/><br/> style="text-align:center;">Garret has demonstrated a strong work ethic, leadership skills, and a positive attitude. He has earned the respect and admiration of his peers and teachers alike. <b - Sarah Schroeder</b>`,
-          `<img src="${homeLogo}" alt="PG Logo"/><br/> style="text-align:center;">He is a trustworthy young man that takes pride in his work and means the best for all he encounters. I am confident that he will be an asset in his future workplace environment. <b - Sarah Schroeder</b>`,
-      
+          `<img src="${homeLogo}" alt="Test 1"/><br/><b style="text-align:center;">Garret has demonstrated a strong work ethic, leadership skills, and a positive attitude. He has earned the respect and admiration of his peers and teachers alike.  - Sarah Schroeder </b>`,
+          `<img src="${homeLogo}" alt="Test 1"/><br/> <b style="text-align:center;">He is a trustworthy young man that takes pride in his work and means the best for all he encounters. I am confident that he will be an asset in his future workplace environment.  - Sarah Schroeder</b>`,
+         
         ],
         autoStart: true,
         loop: true,
