644c2b1d0bd63f0624a84d91e96717d2e26069a7
garbar211 <91475415+garbar211@users.noreply.github.com>
2023-04-27 15:14:07 +0000

    Updated Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -14,7 +14,7 @@ function Footer() {
     <Container fluid className="footer">
       <Row>
         <Col md="4" className="footer-copywright">
-          <h3>Not Finshed </h3>
+          <h3>All images are owned/created by me, expect icons </h3>
         </Col>
         <Col md="4" className="footer-copywright">
           <h3>Copyright © {year}</h3>
