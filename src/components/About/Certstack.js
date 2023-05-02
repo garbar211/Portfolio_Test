5266df1c5dcd447cdc96e69be3cd76f661ac65b2
garbar211 <91475415+garbar211@users.noreply.github.com>
2023-05-02 16:40:37 +0000

    Text size

--- src/components/About/Certstack.js
+++ src/components/About/Certstack.js
@@ -7,18 +7,19 @@ function Toolstack() {
      
       <Col xs={4} md={2} className="tech-icons">
 <img src="https://media.licdn.com/dms/image/C4E0BAQHZvBVomvR5ww/company-logo_100_100/0/1535114460296?e=1691020800&v=beta&t=psdul2Nk1DmlNCWPn2OdH_H5cdMmnJPh6RRtpWJbIYk" />
-        Triple Threat Leadership - Personal Branding 101 - The Branding of ME!
+      <h5>  Triple Threat Leadership - Personal Branding 101 - The Branding of ME!</h5>
+        
 
       </Col>
     
       <Col xs={4} md={2} className="tech-icons">
 <img src="https://media.licdn.com/dms/image/C560BAQHMVfed1djuQA/company-logo_100_100/0/1634816790343?e=1691020800&v=beta&t=ha-QsBg_AZmKQ2q8mMj_9M9PRovl162HWfXqajav2lU" />    
-      Certified in Computer Fundamentals (Foundation)
+     <h5> Certified in Computer Fundamentals (Foundation)</h5>
       </Col>
 
        <Col xs={4} md={2} className="tech-icons">
 <img src="https://media.licdn.com/dms/image/C560BAQHMVfed1djuQA/company-logo_100_100/0/1634816790343?e=1691020800&v=beta&t=ha-QsBg_AZmKQ2q8mMj_9M9PRovl162HWfXqajav2lU" />   
-       Certified in JavaScript Programming
+       <h5>Certified in JavaScript Programming</h5>
        </Col>
       
     </Row>
