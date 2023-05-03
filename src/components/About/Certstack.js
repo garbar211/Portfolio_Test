304cbf7136e909f2330f7e55e6389c71ba6f6e68
garbar211 <91475415+garbar211@users.noreply.github.com>
2023-05-03 15:13:00 +0000

    Added another Cert

--- src/components/About/Certstack.js
+++ src/components/About/Certstack.js
@@ -21,7 +21,10 @@ function Toolstack() {
 <img src="https://media.licdn.com/dms/image/C560BAQHMVfed1djuQA/company-logo_100_100/0/1634816790343?e=1691020800&v=beta&t=ha-QsBg_AZmKQ2q8mMj_9M9PRovl162HWfXqajav2lU" />   
        <h5>Certified in JavaScript Programming</h5>
        </Col>
-      
+       <Col xs={4} md={2} className="tech-icons">
+<img src="https://media.licdn.com/dms/image/C4E0BAQHAzidYsIKzTw/company-logo_100_100/0/1572356795552?e=1691020800&v=beta&t=nlVklyB0A_ImXtqq-j0hUmiiLSnwzGG1lbwyn1Z37CE" />   
+       <h5>FOUNDATIONS IN PERSONAL FINANCE </h5>
+       </Col>
     </Row>
   );
 }
